37746265477baae4e3a9d93ca6347b4cc33dedc5
johnsonsirv <johnsonsirv@gmail.com>
2019-07-31 17:05:55 +0000

    fixed bug with use validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,7 @@ class User < ApplicationRecord
 	validates :username, presence: true, uniqueness: true, length: {in: 8..16}
 	validates :password, presence: true,  length: {in: 8..16}, message: "Please choose a password"
 	validates :email, presence: true, message: "Please enter a valid email"
-	validates :email, :format: { with: VALID_EMAIL_REGEX },
+	validates :email, format: { with: VALID_EMAIL_REGEX },
                     uniqueness: { case_sensitive: false }, message: "Please enter a valid email"
 	
 	
