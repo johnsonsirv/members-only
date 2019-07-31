9ccf9aeb3be2e95f708282c89341ae1b38e16fdd
johnsonsirv <johnsonsirv@gmail.com>
2019-07-31 17:01:56 +0000

    added view for signup, updated user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,20 @@
 class UsersController < ApplicationController
+	def new
+		@user = User.new
+	end
+	
+	def create
+		@user = User.new(user_params)
+		 if @user.save
+			# redirect_to new_user_path
+		else
+			flash.now[:danger] = 'Signup Failed'
+			render :new
+		end
+	end
+	
+	private
+		def user_params
+			params.require(:user).permit(:username, :email, :password_digest)
+		end
 end
