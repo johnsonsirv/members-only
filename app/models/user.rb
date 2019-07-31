b007ae8d23f4fdae0bacbd37a5476b1c161df98a
johnsonsirv <johnsonsirv@gmail.com>
2019-07-31 17:57:57 +0000

    fixed validations bug

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,11 +4,11 @@ class User < ApplicationRecord
 	has_many :posts
 	
 	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-	validates :username, presence: true, uniqueness: true, length: {in: 8..16}
-	validates :password, presence: true,  length: {in: 8..16}, message: "Please choose a password"
-	validates :email, presence: true, message: "Please enter a valid email"
-	validates :email, format: { with: VALID_EMAIL_REGEX },
-                    uniqueness: { case_sensitive: false }, message: "Please enter a valid email"
+	validates :username, presence: {message: "Please choose a username"}, uniqueness: true, length: {maximum: 16, message: "Username too long. Maximum of 16 characters"}
+	validates :password, presence: {message: "Please choose a password"},  length: {in: 8..16, message: "Password length too short or long. 8-16 characters"}
+	validates :email, presence: {message: "Please enter a valid email"} 
+	validates :email, format: { with: VALID_EMAIL_REGEX, message: "Please enter a valid email"  },
+                    uniqueness: { case_sensitive: false }
 	
 	
 end
