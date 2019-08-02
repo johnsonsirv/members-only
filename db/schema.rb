04039f6f7bfddf19365b03cbfcd146dee89d570e
johnsonsirv <johnsonsirv@gmail.com>
2019-08-02 22:36:06 +0000

    added post model, migrated and crate test cases

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_01_222746) do
+ActiveRecord::Schema.define(version: 2019_08_02_222946) do
+
+  create_table "posts", force: :cascade do |t|
+    t.string "title"
+    t.text "body"
+    t.integer "author"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "username"
