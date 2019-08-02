317c8ab865fc65d614d5512f03f9f984b8f91337
johnsonsirv <johnsonsirv@gmail.com>
2019-08-02 22:48:38 +0000

    all post model test pass

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,7 @@
 class Post < ApplicationRecord
 	belongs_to :user
 	
-	validates :title, presence: true, length: {minimum: 1, maximum: 50}
+	validates :title, presence: true, length: {minimum: 1, maximum: 75}
 	validates :body, length: {minimum: 1, maximum: 250}
 	
 	def hottest_first
