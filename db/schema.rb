adb4454d1cc8c5f0c3bb4c8c7f85c192c3eefb65
johnsonsirv <johnsonsirv@gmail.com>
2019-08-02 22:47:26 +0000

    modify schema:

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_02_222946) do
+ActiveRecord::Schema.define(version: 2019_08_02_224419) do
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.text "body"
-    t.integer "author"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
