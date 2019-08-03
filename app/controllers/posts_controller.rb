844a0e3d03299444a0c53e5b17310fe7945e8a3c
johnsonsirv <johnsonsirv@gmail.com>
2019-08-03 14:23:57 +0000

    build post from current user, display post with author to logged_in members

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,19 +2,31 @@ class PostsController < ApplicationController
   before_action :authenticate_user, only: [:new, :create, :destroy]
 	
 	def index
-		
+		if logged_in?
+			@post = Post.joins(:user).select(:title, :body, :username)
+		else
+				@post = Post.select(:title, :body)	
+		end
   end
 	
 	def new
-		
+		@post = Post.new
   end
 	
 	def create
-		
+		@post = current_user.posts.build(title: params[:title], body: params[:body])
+		if post.save
+			flash[:message] = "New Post succesfuly added"
+			redirect_to @post
+		else
+			render :new
+		end
 	end
 	
 	def destroy
-		
+		Post.find_by(params[:id]).destroy
+		flash[:message] = "Post Deleted"
+		redirect_to posts_path
 	end
 	
 	def authenticate_user
@@ -23,6 +35,12 @@ class PostsController < ApplicationController
 			redirect_to login_path
 		end
 	end
-			
+	
+	private
+	
+		
+		
+		def is_current_user?(user)
+		end
 
 end
--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -1,21 +1,26 @@
 require 'rails_helper'
 
 RSpec.describe PostsController, type: :controller do
-
+	before do
+		Post.create(title: "Test Post title", body: "Test post content", user_id: 1)
+	end
+	
+	describe "before_filters" do
+		it { should use_before_action(:authenticate_user) }
+	end
+	
   describe "GET #new" do
-    it "returns http success" do
+    it "returns http redirect unless logged_in" do
       get :new
-      expect(response).to have_http_status(:success)
+      expect(response).to have_http_status(:redirect)
     end
   end
 	
-	 describe "GET #index" do
+	 describe "GET #posts" do
     it "returns http success" do
       get :index
       expect(response).to have_http_status(:success)
     end
   end
-	describe "before_filters" do
-		it { should use_before_action(:authenticate_user) }
-	end
+	
 end
